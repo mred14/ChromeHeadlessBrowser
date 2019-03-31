c04286668a527effbb73637ae6e525097905c591
mred14 <vmadanqa@gmail.com>
2019-03-31 19:42:33 +0530

    Removed Imports

--- src/test/java/ChromeHeadlessTest.java
+++ src/test/java/ChromeHeadlessTest.java
@@ -3,7 +3,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.io.File;
 
 public class ChromeHeadlessTest {
     public static void main(String[] args){
